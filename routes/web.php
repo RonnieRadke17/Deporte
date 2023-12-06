432ee122d4fc1036c86d4f665469dd3db3096c01
RonnieRadke17 <alexisemmanuelzombie@gmail.com>
2023-12-05 22:45:08 -0600

    commit con empleado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmpleadoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::resource('empleado',EmpleadoController::class);
 Route::get('/', function () {
     return view('welcome');
 });
